19fcd1a60cf34187d2b0001852ec76ddbd73a9eb
tduncan14 <nbatre1@yahoo.com>
2024-02-24 18:54:59 -0800

    keep moving forward

---
+++ propertyrental/app/layout.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import '@/assets/styles/global.css'
+
+export const metadata = {
+    title: 'PropertyRent | Find the Perfect Home',
+    description: 'Find your dream rental property',
+    keywords: 'rental,find rentals,find properties'
+}
+
+
+const MainLayout = ({ children }) => {
+
+    return (
+        <html lang='en'>
+            <body>
+                <div>{children}</div>
+            </body>
+        </html>
+    )
+
+}
+
+
+
+export default MainLayout
---
+++ propertyrental/app/page.jsx
@@ -0,0 +1,16 @@
+import react from 'react';
+// export const metadata = {
+//     title: 'Homepage'
+// }
+
+
+const HomePage = () => {
+
+
+
+    return <div>Homepage</div>
+
+}
+
+
+export default HomePage
