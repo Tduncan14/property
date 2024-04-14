0d0125efb27b42554cbdbc0d5868f3ee5e7c2bef
tduncan14 <nbatre1@yahoo.com>
2024-04-13 21:05:00 -0700

    keep moving forward

--- propertyrental/utils/request.js
+++ propertyrental/utils/request.js
@@ -32,5 +32,28 @@ async function fetchProperties() {
     }
 }
 
+// get single property
 
-export { fetchProperties };
+async function fetchProperty(id) {
+    try {
+        // Handle the case where the domain is not available yet
+        if (!apiDomain) {
+            return null;
+        }
+
+        const res = await fetch(`${apiDomain}/properties/${id}`);
+
+        if (!res.ok) {
+            throw new Error('Failed to fetch data');
+        }
+
+        return res.json();
+    } catch (error) {
+        console.log(error);
+        return null;
+    }
+}
+
+
+
+export { fetchProperties, fetchProperty };
